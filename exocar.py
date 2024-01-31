940bedb54531cf0ba051bd1ec0a5ccaf3d2d939b
pumpume <gallien.stef@gmail.com>
2024-01-31 08:15:22 +0100

    fin

--- exocar.py
+++ exocar.py
@@ -3,42 +3,40 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-st.title("Streamlit : build and share data apps")
+
+# st.title("Streamlit: Build and Share Data Apps")
+st.markdown("## Streamlit: Build and Share Data Apps", unsafe_allow_html=True)
 
 
+link  =  "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_car  =  pd.read_csv(link)
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_car = pd.read_csv(link)
+# creation du bouton de filtre appliqué à la page entière
+selected_continents  =  st.multiselect("Select continents", df_car['continent'].unique(), default = df_car['continent'].unique())
+filtered_df = df_car[df_car['continent'].isin(selected_continents)]
 
-# st.write(df_car)
-# st.line_chart(df_car['hp'])
+# lecture du df entier
+st.write(filtered_df)
 
-
-numeric_columns = df_car.select_dtypes(include=['float64', 'int64'])
-
-
+# heatmap pour recherche de corrélation
 st.write("Recherche de corrélation")
-correlation_matrix = numeric_columns.corr()
-fig,ax=plt.subplots()
+correlation_matrix = filtered_df.select_dtypes(include=['float64', 'int64']).corr()
+fig, ax = plt.subplots()
 sns.heatmap(correlation_matrix, center=0, cmap="vlag", annot = True)
 st.pyplot(fig)
 
+# corrélation négative
 st.write("scatterplot mgp / hp")
-fig, ax = plt.subplots()
-sns.scatterplot(x='mpg', y='hp', data=df_car, ax=ax)
+fig, ax  =  plt.subplots()
+sns.scatterplot(x = 'mpg', y = 'hp', data = filtered_df, ax = ax)
 plt.xlabel('mpg')
 plt.ylabel('hp')
 st.pyplot(fig)
 
+# corrélation possive
 st.write("scatterplot hp / weightlbs")
-fig, ax = plt.subplots()
-sns.scatterplot(x='hp', y='weightlbs', data=df_car, ax=ax)
+fig, ax  =  plt.subplots()
+sns.scatterplot(x = 'hp', y = 'weightlbs', data = filtered_df, ax = ax)
 plt.xlabel('hp')
 plt.ylabel('weightlbs')
 st.pyplot(fig)
-
-
-
-# fig, ax = plt.subplots()
-# fig = sns.pairplot(df_car.select_dtypes(include=['float64', 'int64']))
-# st.pyplot(fig)
